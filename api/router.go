0085e882f7c878fcdfc76ae6bd6a9dc6c91ab20b
Sanjarbek <sanjarbekabdurahmonov123@gmail.com>
2024-09-24 15:15:07 +0500

    Ready

--- api/router.go
+++ api/router.go
@@ -22,13 +22,17 @@ func Router(hand *handler.Handler) *gin.Engine {
 	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 
 	// user
-	user := router.Group("/user")
+	user := router.Group("/api/user")
 	user.Use(middleware.Check)
 	user.Use(middleware.CheckPermissionMiddleware(hand.Enforcer))
 	{
-		user.POST("/user/register")
-		user.POST("/user/login")
-		user.GET("/user/getprofile")
+		user.POST("/register", hand.Register)
+		user.POST("/login", hand.Login)
+		user.GET("/getprofile", hand.GetProfile)
+		user.GET("/all", hand.GetAllUsers)
+		user.PUT("/updateprofile", hand.UpdateProfile)
+		user.PUT("/update", hand.UpdateProfileAdmin)
+		user.DELETE("/delete/:id", hand.DeleteProfile)
 	}
 
 	return router

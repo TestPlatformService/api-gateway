ebcbc0b14cd8b3644225e9c8b8fcf693e0091459
Sanjarbek <sanjarbekabdurahmonov123@gmail.com>
2024-10-04 13:13:51 +0500

    Ready

--- api/handler/handler.go
+++ api/handler/handler.go
@@ -8,6 +8,7 @@ import (
 	"api/genproto/task"
 	"api/genproto/topic"
 	"api/genproto/user"
+	"log"
 	"log/slog"
 
 	"github.com/casbin/casbin/v2"
@@ -31,17 +32,18 @@ type Handler struct {
 
 func CORSMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
+		log.Println("Cors middleware triggered")
 		c.Writer.Header().Set("Content-Type", "application/json")
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "https://api.testuzb.uz")
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
 		c.Writer.Header().Set("Access-Control-Max-Age", "86400")
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, UPDATE")
-		c.Writer.Header().Set("Access-Control-Allow-Headers", "Origin, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, X-Max")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, X-Max")
 		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 
 		if c.Request.Method == "OPTIONS" {
 			c.AbortWithStatus(200)
-			return
+		} else {
+			c.Next()
 		}
-		c.Next()
 	}
 }

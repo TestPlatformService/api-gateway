e46d37819bf035c722605056d9b788933542258a
nurmuhammad701 <nurmuhammadmel@gmail.com>
2024-10-07 14:30:43 +0500

    all

--- api/handler/handler.go
+++ api/handler/handler.go
@@ -10,6 +10,7 @@ import (
 	"api/genproto/user"
 	"log"
 	"log/slog"
+	"strings"
 
 	"github.com/casbin/casbin/v2"
 	"github.com/gin-gonic/gin"
@@ -32,18 +33,26 @@ type Handler struct {
 
 func CORSMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		log.Println("Cors middleware triggered")
-		c.Writer.Header().Set("Content-Type", "application/json")
+		log.Println("CORS middleware triggered")
+
 		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
-		c.Writer.Header().Set("Access-Control-Max-Age", "86400")
-		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, UPDATE")
-		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, X-Max")
 		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, UPDATE")
+		c.Writer.Header().Set("Access-Control-Max-Age", "86400") // 24 soat
+
+		// WebSocket ulanishlari uchun qo'shimcha headerlar
+		if strings.ToLower(c.Request.Header.Get("Connection")) == "upgrade" &&
+			strings.ToLower(c.Request.Header.Get("Upgrade")) == "websocket" {
+			c.Writer.Header().Set("Access-Control-Allow-Origin", c.Request.Header.Get("Origin"))
+			c.Writer.Header().Set("Sec-Websocket-Extensions", c.Request.Header.Get("Sec-Websocket-Extensions"))
+		}
 
 		if c.Request.Method == "OPTIONS" {
-			c.AbortWithStatus(200)
-		} else {
-			c.Next()
+			c.AbortWithStatus(204)
+			return
 		}
+
+		c.Next()
 	}
 }
